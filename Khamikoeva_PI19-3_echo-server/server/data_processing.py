87aa3ade739cc850729a69fae08764d89c40e729
Osetinskiy-pokemon <71440118+Osetinskiy-pokemon@users.noreply.github.com>
2021-04-06 23:27:57 +0300

    Update data_processing.py

--- Khamikoeva_PI19-3_echo-server/server/data_processing.py
+++ Khamikoeva_PI19-3_echo-server/server/data_processing.py
@@ -4,7 +4,7 @@ import yaml
 
 
 class DataProcessing:
-    """Класс для работы с коллекцией пользователей в yaml"""
+    # Класс для работы с коллекцией пользователей в yaml
 
     def __init__(self) -> None:
         self.file_path = "./data/users.yml"
@@ -12,7 +12,7 @@ class DataProcessing:
         self.read_collection()
 
     def read_collection(self):
-        """Чтение данных с файла в self.data"""
+        # Чтение данных с файла в self.data
         with open(self.file_path, "r") as stream:
             data = yaml.safe_load(stream)
             if data is None:
@@ -20,18 +20,16 @@ class DataProcessing:
             self.data = data
 
     def write_collection(self):
-        """Запись данных с self.data в файл"""
+        # Запись данных с self.data в файл
         with open(self.file_path, "w") as stream:
             yaml.dump(self.data, stream)
 
     def user_auth(self, ip: str, password: str) -> Tuple[int, Union[str, None]]:
-        """
-        Метод авторизации пользователя в системе
-
-        1 - авторизация прошла успешно
-        0 - авторизация неудачная
-        -1 - необходима регистрация пользователя
-        """
+        # Авторизация пользователя в системе
+        #-1 - необходима регистрация пользователя
+        #0 - авторизация неудачная
+        #1 - авторизация прошла успешно
+        
         for user in self.data:
             if user["ip_addr"] == ip and user["password"] == password:
                 return 1, user["username"]
@@ -43,6 +41,6 @@ class DataProcessing:
         return -1, None
 
     def user_reg(self, ip: str, password: str, username: str) -> None:
-        """Метод регистрации пользователей"""
+        # Регистрации пользователей
         self.data.append({"ip_addr": ip, "password": password, "username": username})
         self.write_collection()
